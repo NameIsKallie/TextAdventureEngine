2e2618f65f2e87d128777506b5592905141c35e5
NameIsKallie <nameiskallie@gmail.com>
2022-07-18 16:28:55 -0600

    Merge, add SampleFile.xml

---
+++ TextAdventureEngine/TextAdventureEngine/GameState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleIO;
+
+namespace TAEngine
+{
+    public class GameState
+    {
+        public GameState(List<string> options)
+        {
+            this.options = options;
+        }
+
+        private int id;
+        private List<string> options;
+        private List<GameState> gameStates;
+
+        public int PromptForChoiceFromOptions()
+        {
+            return CIO.PromptForMenuSelection(options, false);
+        }
+    }
+}
