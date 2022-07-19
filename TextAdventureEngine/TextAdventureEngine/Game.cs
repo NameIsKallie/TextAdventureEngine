09abd56819e3dd882f72013cd4392f831b505971
NameIsKallie <nameiskallie@gmail.com>
2022-07-18 19:59:32 -0600

    Add Game, Player, and GameLoader Classes

---
+++ TextAdventureEngine/TextAdventureEngine/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAEngine
+{
+    internal class Game
+    {
+        
+        private List<GameState> gameStates;
+        private 
+    }
+}
